c88a79f3a8f0014f9dde62bdd954d6cec771f3ad
sina rasouli <sinarasouli79@gmail.com>
2022-08-13 16:34:49 +0430

    configure the admin page

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import Article
 
 # Register your models here.
-admin.site.register(Article)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'publish', 'create', 'update', 'status']
+    list_filter = ['publish', 'create', 'update', 'status']
+    search_fields = ['title', 'description']
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ['status', '-publish']
+
+
+admin.site.register(Article, ArticleAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 # Create your models here.
+
+
 class Article(models.Model):
     STATUS_CHOICES = [
         ('P', 'Publish'),
@@ -14,3 +16,6 @@ class Article(models.Model):
     create = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return self.title
