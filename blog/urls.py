6961e88223a34186ab61a4fb944f8a493dd36093
sina rasouli <sinarasouli79@gmail.com>
2022-08-14 19:02:52 +0430

    config the templates to show the detail dynamic

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import article_list
+from .views import article_list, about, post, contact
 app_name = 'blog'
 urlpatterns = [
     path('', article_list, name='article_list'),
+    path('about/', about, name='about'),
+    path('post/<slug:slug>/', post, name='post'),
+    path('contact/', contact, name='contact'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Article
 
 # Create your views here.
 
 
 def article_list(request):
-    queryset = Article.objects.all()
+    queryset = Article.objects.filter(status='P')
     context = {'queryset': queryset}
     return render(request, 'index.html', context)
+
+
+def about(request):
+    context = {}
+    return render(request, 'about.html', context)
+
+
+def post(request, slug):
+    obj = get_object_or_404(Article, slug=slug, status='P')
+    context = {'object': obj}
+    return render(request, 'post.html', context)
+
+
+def contact(request):
+    context = {}
+    return render(request, 'contact.html', context)
+
+
