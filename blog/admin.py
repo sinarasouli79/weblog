d1ffb74b697f1afacabd7237b05277801e8dae1a
sina rasouli <sinarasouli79@gmail.com>
2022-08-15 12:37:11 +0430

    create category model and connect it to the view and use jcreate, jupdate

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,30 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Category
 
 # Register your models here.
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['position', 'title', 'slug', 'status']
+    list_filter = ['status']
+    search_fields = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['title', 'publish', 'create', 'update', 'status']
+    list_display = ['title', 'jpublish', 'jcreate',
+                    'jupdate', 'status', 'get_category']
     list_filter = ['publish', 'create', 'update', 'status']
     search_fields = ['title', 'description']
     prepopulated_fields = {'slug': ('title',)}
     ordering = ['status', '-publish']
 
+    def get_category(self, obj):
+        return '، '.join([category.title for category in obj.category.all()])
+    get_category.short_description = 'دسته‌بندی'
 
+    # return 'categories'
 admin.site.register(Article, ArticleAdmin)
--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,22 @@ from extensions.utils import jalali_converter
 # Create your models here.
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=255, verbose_name='عنوان')
+    slug = models.SlugField(max_length=100, unique=True, verbose_name='آدرس')
+    status = models.BooleanField(
+        default=True, verbose_name='آیا نمایش داده شود؟')
+    position = models.IntegerField(verbose_name='پوزیشن')
+
+    class Meta:
+        verbose_name = 'دسته‌بندی'
+        verbose_name_plural = 'دسته‌بندی‌ها'
+        ordering = ['position']
+
+    def __str__(self):
+        return self.title
+
+
 class Article(models.Model):
     STATUS_CHOICES = [
         ('P', 'منتشرشده'),
@@ -11,12 +27,16 @@ class Article(models.Model):
     ]
     title = models.CharField(max_length=255, verbose_name='عنوان')
     slug = models.SlugField(max_length=100, unique=True, verbose_name='آدرس')
+    category = models.ManyToManyField(Category, verbose_name='دسته‌بندی')
     description = models.TextField(verbose_name='محتوا')
     thumbnail = models.ImageField(upload_to='images', verbose_name='تصویر')
-    publish = models.DateTimeField(default=timezone.now, verbose_name='زمان انتشار')
+    publish = models.DateTimeField(
+        default=timezone.now, verbose_name='زمان انتشار')
     create = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ساخت')
-    update = models.DateTimeField(auto_now=True, verbose_name='تاریخ بروزرسانی')
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES, verbose_name='وضعیت')
+    update = models.DateTimeField(
+        auto_now=True, verbose_name='تاریخ بروزرسانی')
+    status = models.CharField(
+        max_length=1, choices=STATUS_CHOICES, verbose_name='وضعیت')
 
     class Meta:
         verbose_name = 'مقاله'
@@ -25,6 +45,18 @@ class Article(models.Model):
     def __str__(self):
         return self.title
 
-
     def jpublish(self):
         return jalali_converter(self.publish)
+    jpublish.short_description = 'تاریخ انتشار'
+
+    def jcreate(self):
+        return jalali_converter(self.create)
+    jcreate.short_description = 'تاریخ ساخت'
+
+    def jupdate(self):
+        return jalali_converter(self.update)
+    jupdate.short_description = 'تاریخ بروزرسانی'
+
+    # def get_category(self, *obj):
+    #     # return '،'.join([cat for cat in obj.category.all()])
+    #     return 'categories'
