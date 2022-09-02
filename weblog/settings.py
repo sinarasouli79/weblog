57e5e31b0c6f417a9b82b1c558e625ebfe021c6b
sinarasouli79 <sinarasouli79@gmail.com>
2022-09-02 15:16:30 +0430

    use a free template for login page and configure the static file settings

--- weblog/settings.py
+++ weblog/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
