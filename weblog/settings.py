8187e9309a177797cdb317fdcbf29f6a59a0b412
sinarasouli79 <sinarasouli79@gmail.com>
2022-09-02 15:25:16 +0430

    install django-widget-tweaks and use add_class tag for form inputs

--- weblog/settings.py
+++ weblog/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #third party
+    'widget_tweaks',
     # own
     'blog.apps.BlogConfig',
     'extensions',
