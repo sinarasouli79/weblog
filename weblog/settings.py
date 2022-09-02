624031d0809978530dae7c8854b57445bafd4267
sinarasouli79 <sinarasouli79@gmail.com>
2022-09-02 09:45:26 +0430

    create accounts app and use django auth login view

--- weblog/settings.py
+++ weblog/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
 
     # own
     'blog.apps.BlogConfig',
-    'extensions'
+    'extensions',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'accounts:home'
+LOGIN_URL = 'accounts:login'
