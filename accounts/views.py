b3cff6f864f253214d94a7564f90bf936d0f9bfc
sinarasouli79 <sinarasouli79@gmail.com>
2022-09-03 16:59:37 +0430

    add table to the dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from blog.models import Article
 # Create your views here.
 
 # use listview for representing
@@ -8,4 +8,8 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def home(request):
-    return render(request, 'home.html')
+    articles = Article.objects.all()
+    context = {
+        'object_list': articles
+    }
+    return render(request, 'home.html', context)
