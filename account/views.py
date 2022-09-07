dc4d87bcca963c03ea97d1ebc0b0d844254a0afc
sinarasouli79 <sinarasouli79@gmail.com>
2022-09-07 18:10:12 +0430

    user see self articles and super user see all users articles

--- account/views.py
+++ account/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from blog.models import Article
 # Create your views here.
 
+
 class Home(LoginRequiredMixin, ListView):
-    queryset = Article.objects.all()
     template_name = 'home.html'
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Article.objects.all()
+        else:
+            return Article.objects.filter(author=self.request.user)
