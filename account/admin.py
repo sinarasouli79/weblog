da5e615bfa02bbe752aab8698a26f0cd43dae94b
sinarasouli79 <sinarasouli79@gmail.com>
2022-09-12 18:00:14 +0430

    create is_author field in the account.user
    add the field to the admin.field_sets and display list.

--- account/admin.py
+++ account/admin.py
@@ -3,4 +3,15 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 # Register your models here.
 
+# UserAdmin.fieldsets += (
+#     ("فیلد های خاص من", {"fields": ("is_author",)}),
+# )
+UserAdmin.fieldsets[2][1]['fields'] = ("is_active",
+                                       "is_staff",
+                                       "is_author",
+                                       "is_superuser",
+                                       "groups",
+                                       "user_permissions",
+                                       )
+UserAdmin.list_display += ("is_author", )
 admin.site.register(User, UserAdmin)
--- account/models.py
+++ account/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
 class User(AbstractUser):
-    pass 
+    is_author = models.BooleanField(
+        default=False, verbose_name='وضعیت نویسندگی')
+    
